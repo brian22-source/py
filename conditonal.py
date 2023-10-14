d1652dffef8e293d08a5bff613b2d0b2aae30b4c
19carty <143806608+19carty@users.noreply.github.com>
2023-10-14 21:57:09 +0300

    second commit

---
+++ conditonal.py
@@ -0,0 +1,17 @@
+num = 43
+
+if num >= 43:
+    print("The value of the Num is greater than 32")
+else:
+    print("Num is less than 32")
+
+
+marks = 90
+if marks >= 90:
+    print("Scored an A")
+elif marks >= 80:
+    print("Scored a B")
+elif marks >= 60:
+    print("Scored a C")
+else:
+    print("Failed")
